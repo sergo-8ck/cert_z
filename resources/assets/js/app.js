82560a1787d2007ee1020c3af12f768ac0c928b3
sergo-8ck <sergo-8ck@yandex.ru>
2019-03-20 23:34:26 +0300

    fix dropdown menu

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,3 +4,14 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+
+require('./bootstrap');
+
+require('jquery');
+require('bootstrap');
+
+// //CKEDITOR
+// $( document ).ready(function() {
+// 	CKEDITOR.replace( 'description_short' );
+// 	CKEDITOR.replace( 'description' );
+// });
